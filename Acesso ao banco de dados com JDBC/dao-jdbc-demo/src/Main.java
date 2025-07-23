c60392f71c7a8ea620e7383978ac50b42edb23cc
guilhermeguerradev <guilhermeguerrajf@gmail.com>
2025-07-22 21:48:35 -0300

    findByDepartment implementation and findAll implementation

--- Acesso ao banco de dados com JDBC/dao-jdbc-demo/src/Main.java
+++ Acesso ao banco de dados com JDBC/dao-jdbc-demo/src/Main.java
@@ -1,14 +1,29 @@
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1 : Seller findById ===");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("=== TEST 2 : Seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
+        System.out.println("=== TEST 3 : Seller findAll ===");
+
+
     }
 }
