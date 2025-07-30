912c3c2818d2e27e6de644846e9a8965d79cbb7b
guilhermeguerradev <guilhermeguerrajf@gmail.com>
2025-07-29 22:39:40 -0300

    Project created and User entity and resource

---
+++ Projeto web services com Spring Boot e Jpa. Hibernate/estudoDeSpring/src/main/java/com/guilhermeguerradev/estudoDeSpring/EstudoDeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermeguerradev.estudoDeSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoDeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoDeSpringApplication.class, args);
+	}
+
+}
