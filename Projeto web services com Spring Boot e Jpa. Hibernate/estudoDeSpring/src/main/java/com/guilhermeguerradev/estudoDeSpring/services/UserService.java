4d829e26c7fc5bf6dc332bfa1c5d6d759a0d3c5a
guilhermeguerradev <guilhermeguerrajf@gmail.com>
2025-07-30 23:48:11 -0300

    Service layer, component registration

---
+++ Projeto web services com Spring Boot e Jpa. Hibernate/estudoDeSpring/src/main/java/com/guilhermeguerradev/estudoDeSpring/services/UserService.java
@@ -0,0 +1,24 @@
+package com.guilhermeguerradev.estudoDeSpring.services;
+
+import com.guilhermeguerradev.estudoDeSpring.entities.User;
+import com.guilhermeguerradev.estudoDeSpring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return repository.findById(id);
+    }
+}
